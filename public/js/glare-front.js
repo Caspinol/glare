fd39c79c449479c950864b346fe0c8f564200431
Krzysztof <kristhesavage@gmail.com>
2014-08-30 22:13:43 +0100

    need to fix update now

--- public/js/glare-front.js
+++ public/js/glare-front.js
@@ -3,20 +3,66 @@ $(document).ready(function(){
   var socket = io.connect('localhost:3000');
   
   socket.on('status', function(data){
-    //$('#log_list').append('<li>Status: '+data.message+'</li>');
+    $('#log_list').append('<li>Status: '+data.message+'</li>');
   });
   
   socket.on('tail', function(data){
-    alert("tail : "+data);
     $('#log_list').append('<li>Tail: '+data.logs+'</li>');
   });
   
   socket.on('less', function(data){
-    alert("less" +data.message);
-    $('#log_list').append('<li>Less: '+data.message+'</li>');
+    $('#log_list').append('<li>Less: '+data.logs+'</li>');
     console.log(data.message);
+  });
+
+  $("#submit_cmd").click(function(e){
+    
+    $.ajax({
+      url: '/',
+      method: 'POST',
+      data: $('#command_form').serialize(),
+      dataType: 'text',
+      success: function(data){
+        
+        $('#log_list').empty();
+        
+        if($('#submit_cmd').val() === 'tail'){
+          $('#submit_cmd').val('less');
+          $('#hidden_cmd').val('less');
+        }else{
+          $('#submit_cmd').val('tail');
+          $('#hidden_cmd').val('tail');
+        }
+      }
+    });
+  });
+
+  $('#submit_search').click(function(e){
+    
+    $.ajax({
+      url: '/',
+      method: 'POST',
+      data: $('#grep_form').serialize(),
+      dataType: 'text',
+      success: function(data){
+        $('#log_list').empty();
+      }
+    });
+  });
+
+  $('#submit_file').click(function(e){
+
+    $.ajax({
+      url: '/',
+      method: 'POST',
+      data: $('#file_form').serialize(),
+      dataType: 'text',
+      success: function(data){
+        $('#log_list').empty();
+      }
+    });
+    //e.preventDefault();
   });
 });
 
-var cmd = 
 
--- routes/index.js
+++ routes/index.js
@@ -5,78 +5,77 @@ var log = require('../lib/log');
 /* local definitions */
 var current_grep = "",
 current_file = '/var/log/messages',
-command = 'tail', tail_proc;
+tail_proc;
 
 module.exports = function(app, io){
 
-  var lines;
+  var command = 'tail';  
   /* GET home page */
   app.get('/', function(req, res) {
+    log.info("Got get request!!");
+
+    tail.showDirTree('/var/log', function(files){
+
+      res.render('index',{ 
+	      title: 'Glare',
+	      files: files,
+        current_grep: current_grep,
+        current_file: current_file,
+        command: command
+      });
+    }); 
+  });
+
+  app.post('/', function(req, res){
+    log.info("got post request!!!");
+
+    current_grep = (req.body.search)?req.body.search:current_grep;
+    log.info("Current_grep is: "+ current_grep);
+    current_file = (req.body.filename)?req.body.filename:current_file;
+    log.info("Current_file is: "+ current_file);
+    command = (req.body.command)?req.body.command:command;
+      
+    log.info("Command in request is: "+ req.body.command);
+    log.info("Command was set to: "+ command);
+    
+    if(tail_proc){
+      log.info("Killing tail process: %s", tail_proc);
+      tail.killTail(tail_proc);
+    }
     
     if(command === 'tail'){
+      
+      log.info("Starting new %s process with %s file", command, current_file);  
       tail_proc = tail.doTail(current_file, function(logs){
-        if(logs.indexOf(current_grep) > -1){
-          io.sockets.emit('tail', {logs : logs.toString()});
-        }
-
-        tail.showDirTree('/var/log', function(files){
-          
-          res.render('index', { 
-	          title: 'Glare',
-	          files: files,
-            current_grep: current_grep,
-            current_file: current_file,
-            command: command,
-            lines: lines
-          });
+        lines = logs.split("\n");
+        lines.forEach(function(line){
+          if(line.indexOf(current_grep) > -1){
+            io.sockets.emit('tail', {logs : line});
+          }
         });
-      });
-    }else{
-      if(tail_proc)
-        tail.killTail(tail_proc)
+      }); 
+    }
+    if(command === 'less'){
       
       tail.doLess(current_file, function(logs){
         lines = logs.split("\n");
-        
-        tail.showDirTree('/var/log', function(files){
-          
-          res.render('index', { 
-	          title: 'Glare',
-	          files: files,
-            current_grep: current_grep,
-            current_file: current_file,
-            command: command,
-            lines: lines
-          });
+        lines.forEach(function(line){
+          if(line.indexOf(current_grep) > -1){
+            io.sockets.emit('less', {logs: line});
+          }
         });
       });
     }
-  });
-  
-  app.post('/grep', function(req,res){
-    //kill previous tail
-    if(tail_proc)
-      tail.killTail(tail_proc);
+    //log.info("Redirecting to root!!");
+    tail.showDirTree('/var/log', function(files){
 
-    current_grep = req.body.search;
-    res.redirect('/');
-  });
-
-  app.post('/fileselect', function(req,res){
-    //kill running tail
-    if(tail_proc)
-      tail.killTail(tail_proc);
-
-    current_file = req.body.filename;
-    res.redirect('/');
-  });
-
-  app.post('/command', function(req,res){
-    
-    if(tail_proc)
-      tail.killTail(tail_proc);
-    
-    command = req.body.cmd;
-    res.redirect('/');
+      res.render('index',{ 
+	      title: 'Glare',
+	      files: files,
+        current_grep: current_grep,
+        current_file: current_file,
+        command: command
+      });
+    }); 
   });
 };
